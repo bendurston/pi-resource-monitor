7173b093ffc93b59a9e3d73cf79781efc2bc27f6
Ben Durston <bengdurston@gmail.com>
2021-03-17 12:58:29 -0400

    Fixed call to get_command_output to take an array of arguments.

--- stats-collector/src/main.rs
+++ stats-collector/src/main.rs
@@ -11,11 +11,11 @@ fn main() {
 }
 
 fn runner(){
-    let mut arr: [String; 3]= [String::from("-b"), String::from("-n"), String::from("1")];\
+    let arr: [String; 5]= [String::from("-b"), String::from("-n"), String::from("1"), String::from(""), String::from("")];
     // top -b -n 1 | grep Cpu -> returns best result.
-    let cpu_result: String = get_command_output("top", arr);
+    let cpu_result: String = get_command_output("top", &arr);
     println!("{}", cpu_result);
-    let cpu_final = parse_cpu_stats(&cpu_result);
+    //let cpu_final = parse_cpu_stats(&cpu_result);
 
     //let ram_result: String = get_command_output("free", String::from(""));
 
@@ -34,16 +34,16 @@ fn get_current_time() -> String{
     current_time
 }
 
-fn get_command_output(command: &str, args: &[usize; 2]) -> String{
+fn get_command_output(command: &str, args: &[String; 5]) -> String{
     let output = Command::new(command)   //Execute command, pipes stdio to output
         .args(args)
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .output()
         .expect("Failed to execute");
-    // println!("{:?}", &output.stderr);
+    println!("{:?}", String::from_utf8_lossy(&output.stderr));
     let result = String::from_utf8_lossy(&output.stdout);   // Convert list of bytes to COW
-    println!("{}", result);
+    // println!("{}", result);
     let str_result: String = result.to_string().clone();    // Clones result into a string
     str_result
 }
