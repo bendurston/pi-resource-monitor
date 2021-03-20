e336872ba4703e0408f48c8a8ab40e2748781d9d
Ben Durston <bengdurston@gmail.com>
2021-03-20 09:33:21 -0400

    Removed chrono and function to get current time so the program will be more efficient. Also passed map to send_information function to increase readability.

--- stats-collector/src/main.rs
+++ stats-collector/src/main.rs
@@ -1,11 +1,8 @@
 use std::process;
-use std::error::Error;
 use std::collections::HashMap;
 use std::env;
 use std::{thread, time};
-use chrono;
 use reqwest;
-
 
 
 fn main() {
@@ -29,37 +26,28 @@ fn runner(pi_id: String, url: String, auth: String) {
     // let thirty_seconds = time::Duration::from_micros(30);
     // thread::sleep(thirty_seconds);
     // println!("Slept {} micro-seconds", 30);
+    {
+        let cpu: String = get_program_output("../stats-collector/utils/cpu_runner.sh");
+        let ram: String = get_program_output("../stats-collector/utils/ram_runner.sh");
+        let temperature = String::from("34.6");
+        // let temperature_result: String = get_program_output("../stats-collector/utils/temperature_runner.sh"); Uncomment before build to run on pi (command in temp bash script is not available for linux)
+        
+        let mut map = HashMap::new();
+            map.insert("piId", pi_id);
+            map.insert("cpu", cpu);
+            map.insert("ram", ram);
+            map.insert("temp", temperature);
 
-    let cpu_result: String = get_program_output("../stats-collector/utils/cpu_runner.sh");
-    
-    let ram_result: String = get_program_output("../stats-collector/utils/ram_runner.sh");
-    
-    let time_result: String = get_current_time();
-    // let temperature_result: String = get_program_output("../stats-collector/utils/temperature_runner.sh"); Uncomment before build to run on pi (command in temp bash script is not available for linux)
-
-    send_information(url, auth, pi_id, cpu_result, ram_result, String::from("34.6"), time_result)
+        send_information(url, auth, map);
+    }   // memory occupied by variables and function execution freed.
 }
 
-fn send_information(url: String, _auth: String, pi_id: String, cpu: String, ram: String, temp: String, time: String) {
-    let mut map = HashMap::new();
-        map.insert("PiId", pi_id);
-        map.insert("Time", time);
-        map.insert("cpu", cpu);
-        map.insert("temp", temp);
-        map.insert("ram", ram);
-    
+fn send_information(url: String, _auth: String, map: HashMap<&str, String>) {
     let client = reqwest::blocking::Client::new();
     let _res = client.post(url)
         .json(&map)
         .send()
         .unwrap();
-}
-
-
-fn get_current_time() -> String {
-    let result: chrono::DateTime<chrono::Local> = chrono::Local::now();     // Returns local time of type DateTime
-    let current_time: String = result.to_string().clone();                  // Clones result into a string
-    current_time
 }
 
 fn get_program_output(program: &str) -> String {
